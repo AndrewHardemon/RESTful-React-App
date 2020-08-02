a3bc893a4fd829cb1b42c1be117a50ae6bcab167
Andrew Hardemon <aeroatlas@ymail.com>
2020-08-02 16:35:56 -0700

    added better responses for the requests and added better error handling

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,10 +7,26 @@ const Product = require('../models/product');
 //GET
 router.get('/', (req, res, next) => {
   Product.find() //find all
+    .select('name price _id') //only get these for response
     .exec() //promise
     .then(docs => {
-      console.log(docs);
-      res.status(200).json(docs);
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: 'GET',
+              description: 'Where to get this product',
+              url: 'http://localhost:3000/products/' + doc._id
+            }
+          }
+        })
+      }
+      // (doc.length >= 0) ? 200 ? 404 //optional
+      res.status(200).json(response);
     })
     .catch(err => {
       console.log(err);
@@ -33,8 +49,17 @@ router.post('/', (req, res, next) => {
     .then(result => {
       console.log(result);
       res.status(201).json({
-        message: "Handling GET request to /products",
-        createdProduct: product
+        message: "Created product succesfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price, 
+          _id: result._id,
+          request: {
+            type: 'POST',
+            description: 'Where to get the new product',
+            url: 'http://localhost:3000/products/' + result._id
+          }
+        }
       });
     })
     .catch(err => {
@@ -48,13 +73,19 @@ router.get('/:productId', (req, res, next) => {
   //const id = req.params.productId;
   //Use findById to get the product
   Product.findById(req.params.productId)
+    .select('name price _id') //only get these for response
     .exec() //creates promise
     .then(doc => {
-      console.log("From database",doc);
-      //doc ? res.status(200).json(doc) : res.status(404).json({message: 'No valid entry for provided ID'})
-      if (doc) { res.status(200).json(doc) } //if found
-      else { res.status(404).json({message: 'No valid entry for provided ID'}) } //if not found
-      res.status(200).json(doc);
+      //Terenary Statement for if we found the product or not
+      doc ? res.status(200).json({
+        product: doc,
+        request: {
+          type: 'GET',
+          description: 'Get all products',
+          url: 'http://localhost:3000/products/'
+        }
+      }) : res.status(404)
+        .json({message: 'No valid entry for provided ID'})
     })
     .catch(err => {
       console.log(err)
@@ -64,17 +95,43 @@ router.get('/:productId', (req, res, next) => {
 
 //PATCH/:id
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: "Updated product!"
-  });
+  const updateOps = {};
+  //Run through req.body to find name and value pairs for $set
+  for (const ops of req.body) { //uses object inside of array
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({_id: req.params.productId}, {$set: updateOps})
+    .exec() //creates promise
+    .then( (/*result*/) => {
+      res.status(200).json({
+        message: `Product updated`,
+        request: {
+          type: 'GET',
+          description: 'Get this product',
+          url: 'http://localhost:3000/products/' + req.params.productId
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
+    });
 });
 
 //DELETE/:id
 router.delete('/:productId', (req, res, next) => {
   Product.remove({_id: req.params.productId})
     .exec()
-    .then(result => {
-      res.status(200).json(result);
+    .then((/*result*/) => {
+      res.status(200).json({
+        message: 'Product deleted',
+        request: {
+          type: 'DELETE',
+          description: 'Create new product',
+          body: { name: "String", price: "Number"},
+          url: 'http://localhost:3000/products/'
+        }
+      });
     })
     .catch(err => {
       console.log(err);
--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const mongoose = require('mongoose');
 
+//Getting the routes form our api/routes folder
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//Connect to monngodb atlas or local mongodb using mongoose
+mongoose.connect(process.env.ATLAS_URI 
+  || "mongodb://localhost/restful-react",
+  { useNewUrlParser: true, useUnifiedTopology: true })
+
+//Get rip of Deprecation warning
+mongoose.Promise = global.Promise; //might be redundant
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); //adds body property to requests
+// app.use(cors()); //backup for cors
+
+// //Middleware for preventing CORS errors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); //can replace * with any specific host
+  res.header('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if(req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); //add all requests used by api
+    return res.status(200).json({});
+  }
+  next();
+});
+
+//Routes for handleling routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//Handle requests that don't hit the routes
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404 //returns status 404
+  next(err); //forwards the error request
+});
+
+//Catch all thrown errors (change err to error if issues arise)
+app.use((err, req, res, next) => {
+  res.status(err.status || 500) //500 for other errors
+  res.json({
+    error: {
+      message: err.message
+    }
+  })
+});
 
 module.exports = app;
