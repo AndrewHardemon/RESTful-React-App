0bf66f3d81f08eb5f95e104a8f3daff6a344093c
Andrew Hardemon <aeroatlas@ymail.com>
2020-08-02 11:43:32 -0700

    added mongo, mongoose, cors, model for products, and working routes using mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -11,8 +11,13 @@ router.get('/', (req, res, next) => {
 
 //POST
 router.post('/', (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity
+  }
   res.status(201).json({
-    message: 'Orders was created'
+    message: 'Orders was created',
+    order: order
   });
 });
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,48 +1,87 @@
 //Products Module
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 //GET
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET request to /products"
-  });
+  Product.find() //find all
+    .exec() //promise
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    })
 });
 
 //POST
 router.post('/', (req, res, next) => {
-  res.status(201).json({
-    message: "Handling GET request to /products"
+  //Create product in Mongo with Mongoose
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price
   });
+  product
+    .save() //saves the product using mongoose 
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handling GET request to /products",
+        createdProduct: product
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({error: err})
+    })
 });
 
 //GET/:id
 router.get('/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  if( id === 'special') {
-    res.status(200).json({
-      message: 'You discovered the special ID',
-      id: id
+  //const id = req.params.productId;
+  //Use findById to get the product
+  Product.findById(req.params.productId)
+    .exec() //creates promise
+    .then(doc => {
+      console.log("From database",doc);
+      //doc ? res.status(200).json(doc) : res.status(404).json({message: 'No valid entry for provided ID'})
+      if (doc) { res.status(200).json(doc) } //if found
+      else { res.status(404).json({message: 'No valid entry for provided ID'}) } //if not found
+      res.status(200).json(doc);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: err})
     });
-  } else {
-    res.status(200).json({
-      message: 'You passed an ID'
-    });
-  };
 });
 
 //PATCH/:id
 router.patch('/:productId', (req, res, next) => {
   res.status(200).json({
     message: "Updated product!"
-  })
+  });
 });
 
-//PATCH/:id
+//DELETE/:id
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: "Deleted product!"
-  })
+  Product.remove({_id: req.params.productId})
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error:err
+      });
+    });
 });
 
 module.exports = router
